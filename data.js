ce88441428c6e0a8489b26114c6ea8a2123d7cbe
kuba10155 <j.zborowski24@gmail.com>
2023-01-21 01:29:17 +0100

    Update data.js

--- data.js
+++ data.js
@@ -2,14 +2,14 @@ const characterData = {
   hero: {
       elementId: "hero",
       name: "Wizard",
-      avatar: "/images/wizard.png",
+      avatar: "/wizard.png",
       health: 60,
       diceCount: 3
   },
   monster: {
       elementId: "monster",
       name: "Orc",
-      avatar: "/images/orc.png",
+      avatar: "/orc.png",
       health: 10,
       diceCount: 1
   }
