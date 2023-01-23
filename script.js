e03d3550fe8615ea8064d9578ca9afffb91c5d99
kuba10155 <j.zborowski24@gmail.com>
2023-01-23 03:46:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
 import characterData from './data.js'
 import Character from './Character.js'
 
+let monstersArray = ["orc", "demon", "goblin"]
+let isWaiting = false
+
+function attack() {
+  if(!isWaiting){
+    wizard.setDiceHtml()
+    monster.setDiceHtml()
+    wizard.takeDamage(monster.currentDiceScore)
+    monster.takeDamage(wizard.currentDiceScore)
+    render()
+
+    if(wizard.dead) {
+      endGame()
+    }
+    else if(monster.dead) {
+      isWaiting = true
+      if(monstersArray.length > 0) {
+
+        setTimeout(()=> {
+          monster = getNewMonster()
+          render()
+          isWaiting = false
+        }, 1500)
+      }
+      else {
+        endGame()
+      }
+    }
+  }
+
+}
+
+function getNewMonster() {
+  const nextMonsterData = characterData[monstersArray.shift()]
+  return nextMonsterData ? new Character(nextMonsterData) : {}
+}
+
+function endGame() {
+  isWaiting = true
+  const endMessage = monster.dead && wizard.dead ?
+  "No victors - all creatures are dead"
+    : monster.dead ? "The Wizard Wins"
+    : "The Monsters are Victorious"
+
+  const endEmoji = endMessage === "The Wizard Wins" ? "🔮" : "☠️"
+  setTimeout(() => {
+    document.body.innerHTML = `
+      <div class="end-game">
+        <h2>Game Over</h2>
+        <h3>${endMessage}</h3>
+        <p class="end-emoji">${endEmoji}</p>
+      </div>`
+  }, 1500)
+
+}
+
+document.getElementById('attack-button').addEventListener('click', attack)
+
 function render() {
   document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
-  document.getElementById('monster').innerHTML = orc.getCharacterHtml()
+  document.getElementById('monster').innerHTML = monster.getCharacterHtml()
 }
 
 const wizard = new Character(characterData.hero)
-const orc = new Character(characterData.monster)
+let monster = getNewMonster()
 render()
