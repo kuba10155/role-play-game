aa638b0efb679aea66de8b690a742c520aa2f032
kuba10155 <j.zborowski24@gmail.com>
2023-01-23 03:44:59 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,15 +1,31 @@
 const characterData = {
   hero: {
       name: "Wizard",
-      avatar: "/wizard.png",
+      avatar: "/images/wizard.png",
       health: 60,
-      diceCount: 3
+      diceCount: 3,
+      currentDiceScore: []
   },
-  monster: {
+  orc: {
       name: "Orc",
-      avatar: "/orc.png",
-      health: 10,
-      diceCount: 1
-  }
+      avatar: "/images/orc.png",
+      health: 30,
+      diceCount: 1,
+      currentDiceScore: []
+  },
+    demon: {
+        name: "Demon",
+        avatar: "/images/demon.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "/images/goblin.png",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
+    }
 }
 export default characterData
