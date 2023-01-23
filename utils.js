e547845d7a5d77355d3afd9f968be1f13d884a72
kuba10155 <j.zborowski24@gmail.com>
2023-01-23 03:46:54 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,15 @@
 function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(function() {
-    return Math.floor(Math.random() * 6) + 1
-  })
+  return new Array(diceCount).fill(0).map(() =>
+    Math.floor(Math.random() * 6) + 1)
 }
-export {getDiceRollArray}
+
+function getDicePlaceholderHtml(diceCount) {
+  return new Array(diceCount).fill(0).map(() =>
+    `<div class="placeholder-dice"></div>`).join("")
+}
+
+const getPercentage = (remainingHealth, maximumHealth) =>
+  (remainingHealth * 100) / maximumHealth
+
+export {getDiceRollArray, getDicePlaceholderHtml, getPercentage}
+
